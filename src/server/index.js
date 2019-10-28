bd495d8e62fe0dadc044a85ef7a2dcad547a268b
Lena Morita <lena@jaguchi.com>
2019-10-29 02:33:41 +0900

    Hint private methods in Server

--- src/server/index.js
+++ src/server/index.js
@@ -40,14 +40,12 @@ export default class Socket {
   constructor(url, params) {
     this.url = url;
     this.params = params;
-    this.events = {
+    this._events = {
       message: [
         payload => {
-          if (this.chatStatus !== 'assigned') {
-            this.emit('error', {
-              reason: `'${
-                payload.sender
-              }' attempted to send message, but chat is not active.`
+          if (this._chatStatus !== 'assigned') {
+            this._emit('error', {
+              reason: `'${payload.sender}' attempted to send message, but chat is not active.`
             });
             return;
           }
@@ -55,7 +53,7 @@ export default class Socket {
           debug('Message received: %o', payload);
         }
       ],
-      'chat-status': [event => (this.chatStatus = event.status)]
+      'chat-status': [event => (this._chatStatus = event.status)]
     };
   }
 
@@ -66,49 +64,36 @@ export default class Socket {
     if (!organizationId) {
       delay(() => {
         debug('Failed to connect.');
-        this.emit('error', { reason: 'API Key is invalid.' });
+        this._emit('error', { reason: 'API Key is invalid.' });
       });
       return;
     }
 
     delay(() => {
       debug(
-        `Connected to '${
-          this.url
-        }', organization id ${organizationId} with params %O`,
+        `Connected to '${this.url}', organization id ${organizationId} with params %O`,
         this.params
       );
-      this.emit('connected');
+      this._emit('connected');
 
-      this.assign(organizationId);
+      this._assign(organizationId);
     });
   }
 
-  on(eventName, callback) {
-    const existingCallbacks = this.events[eventName] || [];
-    this.events[eventName] = [...existingCallbacks, callback];
-  }
-
-  off(eventName, callback) {
-    this.events[eventName] = this.events[eventName].filter(
-      existingCallback => existingCallback !== callback
-    );
-  }
-
-  assign(organizationId) {
+  _assign(organizationId) {
     if (orgs[organizationId].isUnavailable) {
-      this.emit('chat-status', { status: 'unavailable' });
+      this._emit('chat-status', { status: 'unavailable' });
       return;
     }
 
-    this.emit('chat-status', { status: 'assigning' });
+    this._emit('chat-status', { status: 'assigning' });
     const operator = findBestOperator(organizationId);
-    this.emit('chat-status', { status: 'assigned', operator });
+    this._emit('chat-status', { status: 'assigned', operator });
   }
 
-  emit(eventName, params) {
-    if (this.events[eventName]) {
-      this.events[eventName].forEach(callback => callback(params));
+  _emit(eventName, params) {
+    if (this._events[eventName]) {
+      this._events[eventName].forEach(callback => callback(params));
     }
 
     if (this.handleEvent) {
